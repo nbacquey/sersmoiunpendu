d9dbe227947664503a8477e05d01ee565aa1161e
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2018-05-10 16:00:22 +0200

    Step by step graphics output.

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "graphics.h"
 
 void testA(){
-  
+
+	int num_step = 11 ; 
+	for (int step_idx = 0; step_idx < num_step; ++step_idx){
+		getchar();
+		system("clear");
+		printf("%s\n", graphics[step_idx]);
+
+	}
 }
 
 void testN(){
